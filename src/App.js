33f2e2dcf29b878981edf17ce297cae58fcd9489
Sabier7 <jonatanvargas777@gmail.com>
2024-02-09 17:53:38 +0100

    primera prueba funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react';
+import TrackList from './components/TrackList';
+
+const App = () => {
+    const [tracks, setTracks] = useState([]);
+
+    const handleTrackUpload = (newTrack) => {
+        setTracks([...tracks, newTrack]);
+    };
+
+    return (
+        <div>
+            <h1>Playback Multitrack</h1>
+            <TrackList tracks={tracks} onTrackUpload={handleTrackUpload} />
+            {/* Otros componentes y funcionalidades aquí */}
+        </div>
+    );
+};
 
 export default App;
